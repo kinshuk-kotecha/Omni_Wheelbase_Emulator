e254da3808df8248efb198b0fb29fddccf8577e8
kinshuk_kotecha <51903279+Kinshuk07@users.noreply.github.com>
2020-08-19 21:33:22 +0800

    helper functions

---
+++ helper.cpp
@@ -0,0 +1,34 @@
+#include "helper.h"
+
+Vec2 polar2vec(const PolarVec p) {
+    float s = sin(p.theta);
+    float c = cos(p.theta);
+    return (Vec2) {
+            .x = p.r * c,
+            .y = p.r * s,
+    };
+}
+
+XYTheta RTOmega2XYTheta(const RTOmega p) {
+    float s = sin(p.v.theta);
+    float c = cos(p.v.theta);
+    return (XYTheta) {
+            .x = p.v.r * c,
+            .y = p.v.r * s,
+            .theta = p.a,
+    };
+}
+
+PolarVec polar_mul(const float k, const PolarVec p) {
+    return (PolarVec) {
+            .r = k * p.r,
+            .theta = p.theta,
+    };
+}
+
+Vec2 addv2(const Vec2 u, const Vec2 v) {
+    return (Vec2) {
+            .x = u.x + v.x,
+            .y = u.y + v.y,
+    };
+}
--- helper.h
+++ helper.h
@@ -1,6 +1,9 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <QElapsedTimer>
+#include <QtMath>
+
 #define sin45 (0.707107)
 #define cos45 (0.707107)
 
@@ -18,5 +21,25 @@ struct PolarVec {
 struct WheelSpeed {
     PolarVec wheels[4];
 };
+
+struct Vec2 {
+    float x;
+    float y;
+};
+
+struct RTOmega {
+    PolarVec v;
+    float a; //angle -- equivalent to XYTheta's theta -- angle of rotation
+};
+
+extern QElapsedTimer *TIME;
+
+#define get_ticks() TIME->elapsed()
+
+Vec2 polar2vec(const PolarVec p);
+XYTheta RTOmega2XYTheta(const RTOmega p);
+PolarVec polar_mul(const float k, const PolarVec p);
+Vec2 addv2(const Vec2 u, const Vec2 v);
+
 
 #endif // HELPER_H
