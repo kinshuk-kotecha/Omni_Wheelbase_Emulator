e48a9e7e7f484e4145e04408288868d1df5478de
kinshuk_kotecha <51903279+Kinshuk07@users.noreply.github.com>
2020-08-20 12:29:36 +0800

    minor fixes

--- Emulator.cpp
+++ Emulator.cpp
@@ -11,13 +11,13 @@ Emulator::Emulator(QTimer* timer) {
 
 void Emulator::generate_trapazoid(const float &acc_limit, const XYTheta &target, Wheelbase& wheelbase) {
     static float START_VEL = 0;
-    static Vec2 cur_pos = {0};
-    static RTOmega cur_vel = {0};
+    static Vec2 cur_pos = {0,0};
+    static RTOmega cur_vel = {{0,0},0};
     static float last_tick = 0;
-    if (get_ticks() - last_tick > 100) {
+    if (get_ticks() - last_tick > 30) {
         last_tick = get_ticks();
         cur_pos = wheelbase.get_pos();
-        cur_vel = {0};
+        cur_vel = {{0,0},0};
     }
     float dT = (float)(get_ticks() - last_tick) / 1000.0;
     float err_x = target.x - cur_pos.x;
@@ -38,18 +38,24 @@ void Emulator::generate_trapazoid(const float &acc_limit, const XYTheta &target,
     cur_pos = addv2(cur_pos, vdt);
 
     last_tick = get_ticks();
-    START_VEL = cur_vel.v.r;
+    //START_VEL = cur_vel.v.r;
+
+    if (fabs(cur_pos.x - target.x) < 5 && fabs(cur_pos.y - target.y) < 5) {
+        wheelbase.set_vel({0,0,0});
+        return;
+    }
 
     wheelbase.set_vel(RTOmega2XYTheta(cur_vel));
-    //qDebug() << cur_vel.v.r;
+    XYTheta debug = RTOmega2XYTheta(cur_vel);
+//    qDebug() << err_x << " " << err_y;
 }
 
 void Emulator::blah() {
     static XYTheta target = {
         .x = 500,
-        .y = 10,
+        .y = 100,
         .theta = 0,
     };
-    generate_trapazoid(2,target, *wheelbase);
+    generate_trapazoid(100,target, *wheelbase);
     wheelbase->move();
 }
